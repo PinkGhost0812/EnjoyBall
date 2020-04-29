f4874e99419116d00338004125e8655ea787816e
PinkGhost0812 <631530326@qq.com>
2020-04-29 11:48:34 +0800

    日你大坝pdt

---
+++ Android/EnjoyBall/app/src/main/java/com/example/lenovo/Util/NewsAndAuthor.java
@@ -0,0 +1,28 @@
+package com.example.lenovo.Util;
+
+import com.example.lenovo.entity.News;
+import com.example.lenovo.entity.User;
+
+public class NewsAndAuthor{
+	private News news;
+	private User author;
+
+	public News getNews() {
+		return news;
+	}
+
+	public void setNews(News news) {
+		this.news = news;
+	}
+
+	public User getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(User author) {
+		this.author = author;
+	}
+	
+	
+	
+}
---
+++ Android/EnjoyBall/app/src/main/java/com/example/lenovo/Util/NewsAndCommentNum.java
@@ -0,0 +1,26 @@
+package com.example.lenovo.Util;
+
+import com.example.lenovo.entity.News;
+
+public class NewsAndCommentNum {
+    private News news;
+    private int commentNum;
+
+    public News getNews() {
+        return news;
+    }
+
+    public void setNews(News news) {
+        this.news = news;
+    }
+
+    public int getCommentNum() {
+        return commentNum;
+    }
+
+    public void setCommentNum(int commentNum) {
+        this.commentNum = commentNum;
+    }
+
+
+}
