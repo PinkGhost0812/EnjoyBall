6838a9790623943496dac5bec4465793f78a80f8
zhengwentaozwt <1678066346@qq.com>
2020-04-30 18:54:43 +0800

    感谢cctv

---
+++ Android/EnjoyBall/app/src/main/java/com/example/lenovo/Activity/IdentityActivity3.java
@@ -0,0 +1,15 @@
+package com.example.lenovo.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.lenovo.enjoyball.R;
+
+public class IdentityActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_identity3);
+    }
+}
