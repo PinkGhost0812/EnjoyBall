0861567a0de36e0920bda25beac9c0390b8adc96
zhengwentaozwt <1678066346@qq.com>
2020-06-02 20:24:01 +0800

    感谢cctv

--- Android/EnjoyBall/app/src/main/java/com/example/lenovo/enjoyball/GuessFragment.java
+++ Android/EnjoyBall/app/src/main/java/com/example/lenovo/enjoyball/GuessFragment.java
@@ -27,7 +27,7 @@ public class GuessFragment extends Fragment{
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_battle_array,container,false);
+        View view = inflater.inflate(R.layout.fragment_guess,container,false);
         listView = view.findViewById(R.id.lv_contestDetail_guess);
         t1 = getArguments().getInt("team1");
         t2 = getArguments().getInt("team2");
