bb81f8cc1bfb63b8926c7af629f268b61b25c3be
liuyongtai <1912310822qq.com>
2020-06-02 20:39:48 +0800

    game

--- Android/EnjoyBall/app/src/main/java/com/example/lenovo/Adapter/GameAdapter.java
+++ Android/EnjoyBall/app/src/main/java/com/example/lenovo/Adapter/GameAdapter.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -137,6 +138,7 @@ public class GameAdapter extends BaseAdapter {
         AddGameDiaglog addGameDiaglog = new AddGameDiaglog();
         if (!addGameDiaglog.isAdded()){
             Bundle bundle = new Bundle();
+            Log.e("比赛id",dataSource.get(i).getContest().getGame_id().toString());
             bundle.putInt("gameid",dataSource.get(i).getContest().getGame_id());
             bundle.putString("teama",dataSource.get(i).getTeamMap().get("nameA").toString());
             bundle.putString("teamb",dataSource.get(i).getTeamMap().get("nameB").toString());
